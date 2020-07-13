9740b2eee06e9cc6972711eee06556393e7ecce8
Kevin Hermann <hermannkevin@gmail.com>
2020-07-13 23:01:14 +0200

    added ui option to exclude directories;

--- ziton/widgets/preferences.py
+++ ziton/widgets/preferences.py
@@ -3,12 +3,12 @@ Widget that represents the preferences submenu.
 """
 from PySide2.QtCore import QFile, Qt
 from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import (QCheckBox, QListWidget, QListWidgetItem,
-                               QPushButton)
+from PySide2.QtWidgets import QCheckBox, QListWidget, QListWidgetItem, QPushButton
 
 from .. import PREFERENCES_PATH
 from .. import config as cfg
 
+# TODO excluded directory seems to be buggy
 
 class PreferenceDialog:
     "Represents the settings dialog."
@@ -23,7 +23,10 @@ class PreferenceDialog:
         # widgets
         self.add_item_btn = self.window.findChild(QPushButton, "add_item")
         self.remove_item_btn = self.window.findChild(QPushButton, "remove_item")
-        self.directory_list = self.window.findChild(QListWidget, "included_files")
+        self.excluded_add_btn = self.window.findChild(QPushButton, "excluded_add_btn")
+        self.excluded_rm_btn = self.window.findChild(QPushButton, "excluded_remove_btn")
+        self.included_list = self.window.findChild(QListWidget, "included_files")
+        self.excluded_list = self.window.findChild(QListWidget, "excluded_files")
         self.exluded_live_dirs = self.window.findChild(QListWidget, "excluded_dirs")
         self.update_startup_box = self.window.findChild(QCheckBox, "update_startup_box")
         self.live_indexing_box = self.window.findChild(QCheckBox, "live_indexing_box")
@@ -35,11 +38,15 @@ class PreferenceDialog:
         # signals
         self.add_item_btn.clicked.connect(self.insert_row)
         self.remove_item_btn.clicked.connect(self.remove_selected)
+        self.excluded_add_btn.clicked.connect(self.excluded_insert_row)
+        self.excluded_rm_btn.clicked.connect(self.excluded_remove_selected)
         self.save_btn.clicked.connect(self.save_configuration)
         self.close_btn.clicked.connect(self.window.accept)
         # load existing config
         for directory in cfg.included_directories():
             self.insert_row(directory)
+        for directory in cfg.excluded_files():
+            self.excluded_insert_row(directory)
         self.update_startup_box.setChecked(cfg.start_updated_enabled())
         self.live_indexing_box.setChecked(cfg.is_indexing_enabled())
         self.hidden_indexing_box.setChecked(cfg.hidden_files_enabled())
@@ -52,20 +59,37 @@ class PreferenceDialog:
         new_item = QListWidgetItem()
         new_item.setText(path)
         new_item.setFlags(new_item.flags() | Qt.ItemIsEditable)
-        i = self.directory_list.count()
-        self.directory_list.insertItem(i, new_item)
+        i = self.included_list.count()
+        self.included_list.insertItem(i, new_item)
+
+    def excluded_insert_row(self, path=False):
+        "Insert a new row into the exlcudex directory table."
+        path = "<new directory>" if not path else path
+        new_item = QListWidgetItem()
+        new_item.setText(path)
+        new_item.setFlags(new_item.flags() | Qt.ItemIsEditable)
+        i = self.excluded_list.count()
+        self.excluded_list.insertItem(i, new_item)
 
     def remove_selected(self):
         "Removes a row."
-        selected = self.directory_list.selectedItems()
+        selected = self.included_list.selectedItems()
         for sel in selected:
-            self.directory_list.takeItem(self.directory_list.row(sel))
+            self.included_list.takeItem(self.included_list.row(sel))
+
+    def excluded_remove_selected(self):
+        "Removes a row from the excluded directory table."
+        selected = self.excluded_list.selectedItems()
+        for sel in selected:
+            self.excluded_list.takeItem(self.excluded_list.row(sel))
 
     def save_configuration(self):
         "writes configuraton changes to disk."
         dirs = [
-            self.directory_list.item(i).text()
-            for i in range(self.directory_list.count())
+            self.included_list.item(i).text() for i in range(self.included_list.count())
+        ]
+        excluded_dirs = [
+            self.excluded_list.item(i).text() for i in range(self.excluded_list.count())
         ]
         current_config = {
             "included_directories": dirs,
@@ -73,7 +97,7 @@ class PreferenceDialog:
             "live_updates": self.live_indexing_box.isChecked(),
             "hidden_files": self.hidden_indexing_box.isChecked(),
             "database_path": cfg.database_path(),
-            "excluded": cfg.excluded_files(),
+            "excluded": excluded_dirs,
         }
         cfg.save_configuration(current_config)
         # close the dialog
